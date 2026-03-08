e3f94e3652605e087f5629c4dbc458a3c925b5c2
shihab-5 <shihabuli364@gmail.com>
2026-03-09 03:09:12 +0600

    dymically api's all section sommit

--- script/home.js
+++ script/home.js
@@ -3,26 +3,39 @@ const showAll=()=>{
     .then((res)=> res.json())
     .then((data)=> displayAll(data.data))
 }
+// "id": 1,
+// "title": "Fix navigation menu on mobile devices",
+// "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+// "status": "open",
+// "labels": [
+// "bug",
+// "help wanted"
+// ],
+// "priority": "high",
+// "author": "john_doe",
+// "assignee": "jane_smith",
+// "createdAt": "2024-01-15T10:30:00Z",
+// "updatedAt": "2024-01-15T10:30:00Z"
 const displayAll=(data)=>{
     const allCont=document.getElementById("container")
     allCont.innerHTML=``
     data.forEach(l => {
         const div=document.createElement("div")
         div.innerHTML=`
-            <div class="p-2 bg-base-100 flex flex-col gap-4 m-2 shadow rounded-[10px] border-t-5 border-t-green-700">
+            <div class="min-h-full p-2 bg-base-100 flex flex-col gap-4 mt-5 shadow rounded-[10px] border-t-5 border-t-green-700">
             <div class="flex justify-between">
            <img src="./assets/Open-Status.png" alt="">
-           <p class="px-5 bg-red-200 text-red-700 font-bold rounded-2xl">high</p>
+           <p class="px-5 bg-red-200 text-red-700 font-bold rounded-2xl">${l.priority}</p>
             </div>
-            <p class="font-bold text-[18px] ">Fix navigation menu on mobile devices</p>
-            <p class=" text-gray-500 text-[15px]">The navigation menu doesn't collapse properly on mobile devices...</p>
+            <p class="font-bold text-[18px] ">${l.title}</p>
+            <p class=" text-gray-500 text-[15px]">${l.description}</p>
             <div class="flex gap-4">
-                 <p class="px-5 bg-red-200 text-red-700 font-bold rounded-2xl"><i class="fa-solid fa-bug"></i> bug</p>
-                  <p class="px-5 bg-orange-200 text-orange-700 font-bold rounded-2xl"><i class="fa-solid fa-life-ring"></i> help wanted</p>
+                 <p class="px-5 bg-red-200 text-red-700 font-bold rounded-2xl"><i class="fa-solid fa-bug"></i> ${l.labels[0]}</p>
+                  <p class="px-5 bg-orange-200 text-orange-700 font-bold rounded-2xl"><i class="fa-solid fa-life-ring"></i>  ${l.labels[1] ?l.labels[1]:``}</p>
             </div>
             <hr class=" border-gray-400 border-t-2 mx-0">
-            <p class="text-gray-500">#1 by john_doe</p>
-            <p class="text-gray-500">1/15/2025</p>
+            <p class="text-gray-500">#${l.id} by ${l.author}</p>
+            <p class="text-gray-500">${l.createdAt}</p>
 
         </div>`
         allCont.appendChild(div)
